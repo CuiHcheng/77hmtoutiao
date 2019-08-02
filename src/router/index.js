6ac4fcd25fb6e67f7bf96f180cb6e6317b32e7ad
cheng <295025257@qq.com>
2019-08-02 20:18:37 +0800

    页面大布局和欢迎页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,23 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    {
+      path: '/',
+      // name: 'home',
+      component: Home,
+      // redirect: '/welcome',
+      children: [
+        { path: '/welcome', name: 'welcome', component: Welcome }
+      ]
+    }
   ]
 })
 
